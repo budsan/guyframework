a052344e1c49416fdf1ee8f2e005773cfe6d99e2
Jordi Santiago Provencio <metalzilla@gmail.com>
2011-10-19 14:02:17 +0200

    Adding 2d ray traversal algorithm to math library WIP.

--- math/ray.h
+++ math/ray.h
@@ -40,6 +40,85 @@ typedef ray<vec3<unsigned long> >  ray3ul;
 typedef ray<vec3<float> >          ray3f;
 typedef ray<vec3<double> >         ray3d;
 
+
+//Based on: Fast Voxel Traversal Algorithm for Ray Tracing
+//By: John Amanatides at al.
+template<typename Real>
+class grid2_traversal
+{
+public:
+	grid2_traversal(const vec2<Real> &gridsize,
+			const vec2<Real> &orig,
+			const vec2<Real> &dest)
+		: abort(false), gridsize(gridsize) {
+		ray2.p = orig;
+		ray2.v = dest - orig;
+		tmax = ray2.v.module();
+		ray2.v.normalize();
+	}
+
+	grid2_traversal(const vec2<Real> &gridsize,
+			const ray<vec2<Real> > &_r2,
+			Real tmax)
+		: abort(false), tmax(tmax), gridsize(gridsize) {
+		ray2 = _r2;
+		ray2.v.normalize();
+	}
+
+	void run()
+	{
+		vec2<Real> invgs(1.0/gridsize.x, 1.0/gridsize.y);
+		vec2i vox(floor(ray2.p.x*invgs.x), floor(ray2.p.y*invgs.y));
+		vec2i step(((ray2.v.x*invgs.x >= 0)<<1)-1, //(r2.v.x >= 0)?+1:-1;
+			   ((ray2.v.y*invgs.y >= 0)<<1)-1);//(r2.v.x >= 0)?+1:-1;
+
+		vec2<Real> tmaxc(tmax,tmax), tdelta;
+		if (ray2.v.x != 0)
+		{
+			tdelta.x = tmax/ray2.v.x;
+			tmaxc.x = (vox.x - ray2.p.x) * tdelta.x;
+		}
+
+		if (ray2.v.y != 0)
+		{
+			tdelta.y = tmax/ray2.v.y;
+			tmaxc.y = (vox.y - ray2.p.y) * tdelta.y;
+		}
+
+		Real tcurr = 0;
+		while(!abort && tcurr < tmax)
+		{
+			if(tmaxc.x < tmaxc.y)
+			{
+				tcurr = tmaxc.x;
+				tmaxc.x = tmaxc.x + tdelta.x;
+				vox.x  = vox.x  + step.x;
+			}
+			else
+			{
+				tcurr = tmaxc.x;
+				tmaxc.y = tmaxc.y + tdelta.y;
+				vox.y  = vox.y  + step.y;
+			}
+			next(vox, tcurr);
+		}
+
+	}
+
+protected:
+
+	void exit() { abort = true; }
+	virtual void next(const vec2i &voxel, Real tcurr) = 0;
+	const ray<vec2<Real> > &ray() {return ray2;}
+
+private:
+
+	bool abort;
+	Real tmax;
+	vec2<Real> gridsize;
+	ray<vec2<Real> > ray2;
+};
+
 } //namespace math
 
 #endif
