b8ccd6aac890194b85add49634beecaca926bfe3
Jordi Santiago Provencio <metalzilla@gmail.com>
2012-02-06 14:05:18 +0100

    More fixes to mat4.

--- math/mat4.h
+++ math/mat4.h
@@ -13,47 +13,50 @@ struct mat4
 {
 	T val[16];
 
-	mat4()
-	{
-
-	}
-
-	mat4(T val[16])
-	{
-		*this = val;
-	}
-
-	mat4(const mat4 &mat)
-	{
-		*this = mat;
-	}
+	mat4() {}
+	mat4(const T val[16]) {*this = val;}
+	mat4(const mat4 &mat) {*this = mat;}
 
 	T &operator()(unsigned int x, unsigned int y)
 	{
 		return val[4*x + y];
 	}
 
-	void zero()
+	inline T *operator[](unsigned int i)
 	{
-		val[0] = 0; val[1] = 0; val[2] = 0; val[3] = 0;
-		val[4] = 0; val[5] = 0; val[6] = 0; val[7] = 0;
-		val[8] = 0; val[9] = 0; val[10]= 0; val[11]= 0;
-		val[12]= 0; val[13]= 0; val[14]= 0; val[15]= 0;
+		return &val[4*i];
 	}
 
-	static mat4<T> from_zero()
+	inline const T *operator[](unsigned int i) const
 	{
-		mat4<T> res;
-		res.zero();
-		return res;
+		return &val[4*i];
 	}
 
-	void identity()
+	inline mat4<T> &operator=(const mat4<T>  &m)
+	{
+		return *this = m.val;
+	}
+
+	inline mat4<T> &operator=(const T val[16])
+	{
+		memcpy(this->val, val, 16 * sizeof(T));
+		return *this;
+	}
+
+	void reset()
 	{
 		val[0] = 1; val[1] = 0; val[2] = 0; val[3] = 0;
 		val[4] = 0; val[5] = 1; val[6] = 0; val[7] = 0;
 		val[8] = 0; val[9] = 0; val[10]= 1; val[11]= 0;
 		val[12]= 0; val[13]= 0; val[14]= 0; val[15]= 1;
+	}
+
+	static mat4<T> identity()
+	{
+
+		mat4<T> res;
+		res.reset();
+		return res;
 	}
 
 	T det()
@@ -85,14 +88,6 @@ struct mat4
 			((*this)[0][0]*(*this)[1][1]*(*this)[2][2]*(*this)[3][3]);
 	}
 
-	static mat4<T> from_identity()
-	{
-
-		mat4<T> res;
-		res.identity();
-		return res;
-	}
-
 	mat4<T> operator*(const mat4<T> &mat)
 	{
 		mat4<T> res;
@@ -117,10 +112,9 @@ struct mat4
 		return res;
 	}
 
-	static mat4<T> from_rotate(T angle, T x, T y, T z)
+	static mat4<T> rotation_rad(T angle, T x, T y, T z)
 	{
-		double rad = angle * (M_PI/180);
-		double s = sin(rad), c = cos(rad);
+		double s = sin(angle), c = cos(angle);
 		double ci = 1.0f - c;
 		double mod = sqrt(x*x + y*y + z*z);
 		double invmod = 1.0f / mod;
@@ -131,7 +125,7 @@ struct mat4
 		T xy, yz, xz;
 
 		mat4<T> res;
-		res.identity();
+		res.reset();
 
 		x1 = x * invmod;
 		y1 = y * invmod;
@@ -156,10 +150,16 @@ struct mat4
 		return res;
 	}
 
-	static mat4<T> from_translate(T x, T y, T z)
+	static mat4<T> rotation(T angle, T x, T y, T z)
+	{
+		double rad = angle * (M_PI/180);
+		return rotation_rad(rad, x, y, z);
+	}
+
+	static mat4<T> translation(T x, T y, T z)
 	{
 		mat4<T> res;
-		res.identity();
+		res.reset();
 
 		res[3][0] = x;
 		res[3][1] = y;
@@ -168,10 +168,10 @@ struct mat4
 		return res;
 	}
 
-	static mat4<T> from_scale(T x, T y, T z)
+	static mat4<T> scaling(T x, T y, T z)
 	{
 		mat4<T> res;
-		res.identity();
+		res.reset();
 
 		res[0][0] = x;
 		res[1][1] = y;
@@ -180,10 +180,10 @@ struct mat4
 		return res;
 	}
 
-	static mat4<T> from_frustum(T l, T r, T b, T t, T n, T f)
+	static mat4<T> frustum(T l, T r, T b, T t, T n, T f)
 	{
 		mat4<T> res;
-		res.identity();
+		res.reset();
 
 		res[0][0] = (2.0f * n) / (r - l);
 
@@ -200,10 +200,10 @@ struct mat4
 		return res;
 	}
 
-	static mat4<T> from_ortho(T l, T r, T b, T t, T n, T f)
+	static mat4<T> ortho(T l, T r, T b, T t, T n, T f)
 	{
 		mat4<T> res;
-		res.identity();
+		res.reset();
 
 		res[0][0] = 2.0f / (r - l);
 
@@ -225,25 +225,36 @@ struct mat4
 
 	void rotate(T angle, T x, T y, T z)
 	{
-		*this *= from_rotate(angle, x, y, z);
+		*this *= rotation(angle, x, y, z);
+	}
+
+	void rotate_rad(T angle, T x, T y, T z)
+	{
+		*this *= rotation_rad(angle, x, y, z);
 	}
 
 	void translate(T x, T y, T z)
 	{
-		*this *= from_translate(x, y, z);
+		*this *= translation(x, y, z);
 	}
 
 	void scale(T x, T y, T z)
 	{
-		*this *= from_scale(x, y, z);
+		*this *= scaled(x, y, z);
 	}
 
-	mat4<T> inverse()
+	mat4<T> inverted(bool * invertible = 0)
 	{
 		mat4<T> res;
 		T det = this->det();
 
-		if(det == 0) return; // Singular matrix
+		if(det == 0) // Singular matrix
+		{
+			if (invertible) *invertible = false;
+			return identity();
+		}
+
+		if (invertible) *invertible = true;
 		T invDet = 1/det;
 
 		res[0][0] = invDet *	((-(*this)[1][3]*(*this)[2][2]*(*this)[3][1]) +
@@ -349,7 +360,15 @@ struct mat4
 		return res;
 	}
 
-	void transpose(void)
+	bool inverse()
+	{
+		bool invertible;
+		mat4f inv = this->inverted(&invertible);
+		if (inversible) *this = inv;
+		return invertible;
+	}
+
+	mat4<T> transposed()
 	{
 		mat4<T> mat;
 
@@ -361,28 +380,12 @@ struct mat4
 			mat[3][i] = (*this)[i][3];
 		}
 
-		*this = mat;
+		return mat;
 	}
 
-	inline T *operator[](unsigned int i)
+	void transpose(void)
 	{
-		return &val[4*i];
-	}
-
-	inline const T *operator[](unsigned int i) const
-	{
-		return &val[4*i];
-	}
-
-	inline mat4<T> &operator=(const mat4<T>  &m)
-	{
-		return *this = m.val;
-	}
-
-	inline mat4<T> &operator=(const T val[16])
-	{
-		memcpy(this->val, val, 16 * sizeof(T));
-		return *this;
+		*this = this->transposed();
 	}
 };
 
