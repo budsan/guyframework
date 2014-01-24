79fcf1b21d1bf6132ee5af3762b3784f56e8398a
Nico <nico@codrilo.com>
2014-01-24 13:53:41 +0100

    stupid mistake

--- graphics/sprite.cpp
+++ graphics/sprite.cpp
@@ -119,7 +119,7 @@ void Sprite::draw()
 	model.translate(m_pos.x, m_pos.y, 0.0);
 	model.scale(m_unitsPerPixel*m_scale.x, m_unitsPerPixel*m_scale.y, 0.0);
 	model.rotate(m_rotate, 0.0, 0.0, 1.0);
-	model.translate(static_cast<double>(-params.cx), static_cast<double>(-params.cy), 0);
+	model.translate(static_cast<double>(-params.cx), static_cast<double>(+params.cy), 0);
 
 	glMultMatrixd(model.v);
 
